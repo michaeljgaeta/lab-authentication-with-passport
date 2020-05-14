13170090c1c7d09cb3fbc492b2f16771ee0626fa
michaeljgaeta <michaeljgaeta@gmail.com>
2020-05-14 12:20:01 +0100

    iteration 1 sign up feature

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,39 @@
-'use strict';
+"use strict";
 
-const { Router } = require('express');
+const { Router } = require("express");
 const authenticationRouter = Router();
+
+const passport = require("passport");
+
+// 5 - We need to tell our route handlers to use their corresponding strategies.
+
+authenticationRouter.get("/sign-up", (req, res, next) => {
+  res.render("sign-up", { title: "Sign Up" });
+});
+
+authenticationRouter.post(
+  "/sign-up",
+  passport.authenticate("sign-up", {
+    successRedirect: "/",
+    failureRedirect: "/authentication/sign-up"
+  })
+);
+
+authenticationRouter.get("/sign-in", (req, res, next) => {
+  res.render("sign-in");
+});
+
+authenticationRouter.post(
+  "/sign-in",
+  passport.authenticate("sign-in", {
+    successRedirect: "/",
+    failureRedirect: "/authentication/sign-in"
+  })
+);
+
+authenticationRouter.post("/sign-out", (req, res, next) => {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports = authenticationRouter;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const { Router } = require('express');
-const router = Router();
+const indexRouter = Router();
 
-router.get('/', (req, res, next) => {
+indexRouter.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
 });
 
-module.exports = router;
+module.exports = indexRouter;
